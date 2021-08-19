80c8bd7af4d05131df2ccb5a569855186bfc70a3
Loni <kuangloni@gmail.com>
2021-08-18 17:25:31 -0700

    added doc string for loading message

--- src/Loading.js
+++ src/Loading.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Loading.css";
 
+/** Renders a Loading message during API requests. */
 export default function Loading() {
   return (
     <div className="Loading">
