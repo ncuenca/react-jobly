47666f398bbe05c36f6e692faa003eccb35c3f75
Loni <kuangloni@gmail.com>
2021-08-20 09:56:05 -0700

    accept changes

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -16,15 +16,10 @@ export default function SearchBar({ search, initialTerm }) {
   // useEffect separates request from handleChange
 
   function handleChange(evt) {
-<<<<<<< HEAD
-    setTerm(evt.target.value);
-    search(evt.target.value);
-=======
     setTerm(() => {
       search(evt.target.value.trim());
       return evt.target.value;
     });
->>>>>>> 5cd6ea6d5a913168787cdd3533837da7b750591e
   }
 
   // trim term before passing to search
@@ -34,13 +29,8 @@ export default function SearchBar({ search, initialTerm }) {
   // }
 
   return (
-<<<<<<< HEAD
-    <form onSubmit={handleSubmit} style={{width: "60%"}}>
-      <div className="input-group mb-3 mt-3">
-=======
     <form style={{width: "60%"}}>
       <div className="input-group mb-3">
->>>>>>> 5cd6ea6d5a913168787cdd3533837da7b750591e
         <input 
           type="text"
           value={term} 
